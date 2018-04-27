e21136b68571cb0f5f4aa36edfd74871443aa4cc
mikebpech <mike.pechousek@gmail.com>
2018-04-27 12:25:12 -0400

    Added some functionality with working form + input...

--- server/server.js
+++ server/server.js
@@ -4,24 +4,32 @@ const express = require('express');
 const socketIO = require('socket.io');
 const chalk = require('chalk');
 
+const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log(chalk.blue('New user connected'));
 
-    socket.on('createMessage', (message) => {
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined the chat.'));
+
+    socket.on('createMessage', (message, callback) => {
         console.log('New Message:', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
+        //EMIT TO EVERYONE BUT YOURSELF
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', (socket) => {
